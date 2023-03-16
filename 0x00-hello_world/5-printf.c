859b9bcb97646fa1b06b6ebbb405cce92679d01a
Victor Ibironke <victoribironke2019@gmail.com>
2023-03-16 21:34:12 +0100

    C programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function
+ * Description: A C program that prints with printf function
  *
  * Return: Always 0 (Success)
 */
