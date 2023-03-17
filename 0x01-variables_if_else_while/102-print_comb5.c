755ddf926faaa6a87a78baec26fcabed70418c34
Victor Ibironke <victoribironke2019@gmail.com>
2023-03-17 18:41:12 +0100

    Advanced variables

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num1 = 0, num2;
+
+	while (num1 <= 99)
+	{
+		num2 = num1;
+		while (num2 <= 99)
+		{
+			if (num2 != num1)
+			{
+				putchar((num1 / 10) + 48);
+				putchar((num1 % 10) + 48);
+				putchar(' ');
+				putchar((num2 / 10) + 48);
+				putchar((num2 % 10) + 48);
+
+				if (num1 != 98 || num2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++num2;
+		}
+		++num1;
+	}
+	putchar('\n');
+
+	return (0);
+}
