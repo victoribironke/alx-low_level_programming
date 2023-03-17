a00f3a62d22427d35c400c51fc75113e3d2c0038
Victor Ibironke <victoribironke2019@gmail.com>
2023-03-17 18:45:42 +0100

    Advanced variables

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,39 +1,39 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
- * 
- * Description: print all possible different combinations
- *              of two digits
- * 
- * Return: Always 0 (Sucess)
+ *
+ * Description: print all possible different
+ *           combinations of two digits.
+ *
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-    int num1 = 0, num2;
+	int num1 = 0, num2;
 
-    while (num1 <= 9)
-    {
-        num2 = 0;
-        while (num2 <= 9)
-        {
-            if (num1 != num2 && num1 < num2)
-            {
-                putchar(num1 + 48);
-                putchar(num2 + 48);
+	while (num1 <= 9)
+	{
+		num2 = 0;
+		while (num2 <= 9)
+		{
+			if (num1 != num2 && num1 < num2)
+			{
+				putchar(num1 + 48);
+				putchar(num2 + 48);
 
-                if (num1 + num2 != 17)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-            ++num2;
-        }
-        ++num1;
-    }
-    putchar('\n');
+				if (num1 + num2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++num2;
+		}
+		++num1;
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
